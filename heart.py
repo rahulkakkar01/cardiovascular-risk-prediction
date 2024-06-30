d16a9138be8dca8b9405584d657c5d56ec13dac1
rahulkakkar01 <128950813+rahulkakkar01@users.noreply.github.com>
2024-06-30 15:40:17 +0530

    heart.py

--- heart.py
+++ heart.py
@@ -8,16 +8,12 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, roc_auc_score
 from joblib import dump
 
-# Load Data
 data = pd.read_csv('CVD_cleaned.csv')
 
-# Preprocessing
-# Define categorical and numerical columns
 categorical_features = ['General_Health', 'Checkup', 'Exercise', 'Sex', 'Age_Category', 'Smoking_History','Arthritis']
 
 numerical_features = ['Height_(cm)', 'BMI', 'Fruit_Consumption', 'Alcohol_Consumption', 'Green_Vegetables_Consumption', 'FriedPotato_Consumption']
 
-# Preprocessing pipelines for both numeric and categorical features
 numeric_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='median')),
     ('scaler', StandardScaler())])
@@ -26,34 +22,27 @@ categorical_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='most_frequent')),
     ('onehot', OneHotEncoder(handle_unknown='ignore'))])
 
-# Combine preprocessing steps
 preprocessor = ColumnTransformer(
     transformers=[
         ('num', numeric_transformer, numerical_features),
         ('cat', categorical_transformer, categorical_features)])
 
-# Define model pipeline
 model = Pipeline(steps=[
     ('preprocessor', preprocessor),
     ('classifier', RandomForestClassifier(n_estimators=100, random_state=42))])
 
-# Separate features and target
 X = data.drop('Heart_Disease', axis=1)
 y = data['Heart_Disease']
 
-# Split data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Train the model
 model.fit(X_train, y_train)
 
-# Save the trained model and preprocessor
 dump(model, 'model.joblib')
 dump(preprocessor, 'preprocessor.joblib')
 
-# Evaluate
 y_pred = model.predict(X_test)
-y_pred_proba = model.predict_proba(X_test)[:, 1]  # Probabilities for the positive class
+y_pred_proba = model.predict_proba(X_test)[:, 1]
 
 print(classification_report(y_test, y_pred))
 print("ROC-AUC Score:", roc_auc_score(y_test, y_pred_proba))
